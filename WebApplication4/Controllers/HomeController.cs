46f0944249d5850f62c4c282ddd6c8a5b3aeb725
JW <winchesterjoshuar@gmail.com>
2017-07-27 11:49:26 -0400

    Initial Upload

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Models.Register r)
+        {
+            ViewBag.Message = "Welcome to the Grand Circus Cool Coffee Corp website.";
+            ViewBag.Name = r.FirstName;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Please enter the following information to sign up.";
+            return View();
+        }
+        public ActionResult Summary(Models.Register r)
+        {
+            ViewBag.Message = "Your name is: "  + r.FirstName;
+         
+
+            ViewBag.Email +=  " Your email is: " + r.Email;
+           
+            ViewBag.Gender += "Your gender is: " + r.Gender;
+            ViewBag.Message += r.thePass;
+          
+            return View(r);
+        }
+    }
+}
---
+++ WebApplication4/Models/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Register //so this Register class is an object that can be used. Its a thing in the real world that we can store data in and do stuff with
+
+    {
+
+        [Required]
+
+        [Key]
+
+        public string FirstName { get; set; }
+
+        [Required]
+
+        public string Email { get; set; }
+
+        [Required]
+
+        public int Phone { get; set; }
+
+        public string Gender { get; set; }
+
+        public string userName { get; set;  } 
+
+    public string WellBeing { get; set; }
+
+        [Required]
+        public string thePass { get; set; }
+
+}
+
+}
